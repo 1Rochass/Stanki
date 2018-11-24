68c45a77eae7305b9c695a8d00964e11f315d3c1
1Rochass <rochass@mail.ru>
2018-11-24 19:31:19 +0300

    Bootstrap grid. Woocommerce rusification.

--- wp-content/themes/Stanki/index.php
+++ wp-content/themes/Stanki/index.php
@@ -1,29 +1,36 @@
-<?php get_header(); ?>
-
-<div class="content-main">
-
-  <div class="content">
-
-    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<div class="header row">
+	<div class="header-nav col-md-12">
+		<?php get_header(); ?> <!-- Get header -->
+	</div>
+</div>
+<h1>Main</h1>
+<div class="main row">
+	<div class="main-sidebar d-none d-md-block col-md-3">
+		<?php get_sidebar(); ?> <!-- Get sidebar -->
+	</div>
+	<div class="main-content col-md-9">
+		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
     
-    <div class="post-content">
-      <div class="post-img"><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('full'); ?></a></div>
-      <h2 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-      <div class="post-text"><?php the_excerpt(); ?></div>
-      <div class="readmore"><a href="<?php the_permalink(); ?>">Читать далее</a></div>
-    </div>
+	    <div class="post-content">
+	      <div class="post-img"><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('full'); ?></a></div>
+	      <h2 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+	      <div class="post-text"><?php the_excerpt(); ?></div>
+	      <div class="readmore"><a href="<?php the_permalink(); ?>">Читать далее</a></div>
+	    </div>
 
-    <?php endwhile; ?>
+	    <?php endwhile; ?>
 
-    <?php else: ?>
+	    <?php else: ?>
 
-    <?php endif; ?>
-
-  </div>
-
-  <?php get_sidebar(); ?>
-
+	    <?php endif; ?>
+	</div>
+</div>
+<div class="footer row">
+	<div class="footer-nav col-md-12">
+		<?php get_footer(); ?>
+	</div>
 </div>
 
-<?php get_footer(); ?>
+
+
 
--- wp-content/themes/Stanki/single.php
+++ wp-content/themes/Stanki/single.php
@@ -1,11 +1,15 @@
-<?php get_header(); ?>
-
-<div class="content-main">
-
-  <div class="content">
-    <div id="page">
-
-      <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<div class="header row">
+  <div class="header-nav col-md-12">
+    <?php get_header(); ?> <!-- Get header -->
+  </div>
+</div>
+<h1>Main</h1>
+<div class="main row">
+  <div class="main-sidebar d-none d-md-block col-md-3">
+    <?php get_sidebar(); ?> <!-- Get sidebar -->
+  </div>
+  <div class="main-content col-md-9">
+   <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
       <h1 class="post-title"><?php the_title(); ?></h1>
       <div class="post-text"><?php the_content(); ?></div>
       <?php comments_template(); ?>
@@ -14,11 +18,10 @@
       <?php else: ?>
 
       <?php endif; ?>
-    </div>
   </div>
-
-  <?php get_sidebar(); ?>
-
 </div>
-
-<?php get_footer(); ?>
+<div class="footer row">
+  <div class="footer-nav col-md-12">
+    <?php get_footer(); ?>
+  </div>
+</div>
