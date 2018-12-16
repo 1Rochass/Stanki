0e8f57fb99167fa67565e49ed7d88226536f3724
1Rochass <rochass@mail.ru>
2018-12-16 22:34:12 +0300

    added Woo-ce shortcode and redesigned main page

--- wp-content/themes/Stanki/index.php
+++ wp-content/themes/Stanki/index.php
@@ -5,24 +5,8 @@
 		</div>
 	</div>
 	<div class="main row no-gutters">
-		<div class="main-sidebar d-none d-md-block col-md-3">
-			<?php get_sidebar(); ?> <!-- Get sidebar -->
-		</div>
-		<div class="main-content col-md-9">
-			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-	    
-		    <div class="post-content">
-		      <div class="post-img"><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('full'); ?></a></div>
-		      <h2 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-		      <div class="post-text"><?php the_excerpt(); ?></div>
-		      <div class="readmore"><a href="<?php the_permalink(); ?>">Читать далее</a></div>
-		    </div>
-
-		    <?php endwhile; ?>
-
-		    <?php else: ?>
-
-		    <?php endif; ?>
+		<div class="main-content col-md-12">
+			<?php echo do_shortcode('[products]'); ?>
 		</div>
 	</div>
 
